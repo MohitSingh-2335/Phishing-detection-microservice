3f16164ddef21033a4242527833a3b64e1a5f606
MohitSingh-2335 <2335mohitsingh@gmail.com>
2026-01-25 22:07:34 +0530

    Feat: Completed feature extraction logic using re.search

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,7 +4,12 @@ from urllib.parse import urlparse
 def extract_features(url):
     """
     Converts a URL into numerical features for ML models.
+    Logic: 1 = Risky/Bad, 0 = Safe/Good
     """
+
+    #Ensuring URL starting with HTTPS
+    if not url.startswith(("http://", "https://")):
+        url = 'http://' + url
 
     # Parse URL
     parsed = urlparse(url)
@@ -16,17 +21,18 @@ def extract_features(url):
     dot_count = url.count('.')
 
     # 3. HTTPS check (correct way)
-    is_https = 1 if parsed.scheme == "https" else 0
+    is_not_https = 0 if parsed.scheme == "https" else 1
 
     # 4. IP address in domain
-    has_ip = 1 if re.fullmatch(
-        r'(\d{1,3}\.){3}\d{1,3}', parsed.netloc
+    has_ip = 1 if re.search(
+        r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', parsed.netloc
     ) else 0
 
     # 5. Suspicious keywords
     suspicious_words = [
         'login', 'secure', 'account', 'update',
-        'verify', 'bank', 'signin', 'confirm'
+        'verify', 'bank', 'signin', 'confirm',
+        'wallet', 'crypto', 'free'
     ]
     suspicious_count = sum(
         1 for word in suspicious_words if word in url.lower()
@@ -35,7 +41,7 @@ def extract_features(url):
     return [
         url_length,
         dot_count,
-        is_https,
+        is_not_https,
         has_ip,
         suspicious_count
     ]
